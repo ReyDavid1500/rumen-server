73b0dab3e8083bb5e93d7bea06fd63fe3b3249e6
David Guzman <davidguzman1500@gmail.com>
2023-12-22 16:34:44 -0300

    address entity and validation pipe config

--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAddressDto {}
+import { IsInt, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  userId: string;
+
+  @IsString()
+  street: string;
+
+  @IsInt()
+  number: number;
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  province: string;
+}
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Address } from './address.schema';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Address' })
+  address: Address; //Preguntar a la March!!
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
+
+  @Prop({ default: false })
+  isAdmin: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
