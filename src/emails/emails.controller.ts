2f056458badd2e8bd45f14515eb8b5664258d190
David Guzman <davidguzman1500@gmail.com>
2024-03-27 23:00:15 -0300

    EJS email templeta added and email module updated

--- src/emails/emails.controller.ts
+++ src/emails/emails.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { EmailsService } from './emails.service';
 
 @Controller('emails')
 export class EmailsControllers {
   constructor(private readonly emailsService: EmailsService) {}
 
-  @Get()
+  @Post()
   sendEmail(): void {
     return this.emailsService.sendEmail();
   }
--- src/emails/emails.module.ts
+++ src/emails/emails.module.ts
@@ -2,20 +2,37 @@ import { Module } from '@nestjs/common';
 import { EmailsControllers } from './emails.controller';
 import { EmailsService } from './emails.service';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com',
-        auth: {
-          user: 'davidguzman1500@gmail.com',
-          pass: process.env.GMAIL_APP_PASS,
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          auth: {
+            user: 'davidguzman1500@gmail.com',
+            pass: process.env.GMAIL_APP_PASS,
+          },
         },
-      },
+        defaults: {
+          from: `"Nice App" <${config.get('SMTP_USERNAME')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [EmailsControllers],
   providers: [EmailsService],
+  exports: [EmailsService],
 })
 export class EmailsModule {}
